1cb480e7eee2a18eceb39d4d9ce61c59a9510308
edwinchen85 <edwinchen85@gmail.com>
2017-08-31 23:15:44 +0800

    Create ctrlAddItem()

--- app.js
+++ app.js
@@ -12,14 +12,26 @@ var UIController = (function() {
 
 var controller = (function(budgetCtrl, UICtrl) {
 
-  document.querySelector('.add__btn').addEventListener('click', function() {
+  var ctrlAddItem = function() {
 
-  });
+    // 1. Get the field input data
+
+    // 2. Add the item to the budget controller
+
+    // 3. Add the item to the UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the UI
+
+  }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event) {
 
     if(event.keyCode == 13 || event.which == 13) {
-
+      ctrlAddItem();
     }
 
   });
