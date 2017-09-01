3999a7b51046ef6b08e515c04f8f3167fe8ea5d9
edwinchen85 <edwinchen85@gmail.com>
2017-09-01 21:27:21 +0800

    Create an initialization function

--- app.js
+++ app.js
@@ -31,13 +31,25 @@ var UIController = (function() {
 
 var controller = (function(budgetCtrl, UICtrl) {
 
-  var DOM = UICtrl.getDOMstrings();
+  var setupEventListener = function() {
+    var DOM = UICtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+
+      if(event.keyCode == 13 || event.which == 13) {
+        ctrlAddItem();
+      }
+
+    });
+  };
+
 
   var ctrlAddItem = function() {
 
     // 1. Get the field input data
     var input = UICtrl.getinput();
-    console.log(input);
 
     // 2. Add the item to the budget controller
 
@@ -47,16 +59,15 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     // 5. Display the budget on the UI
 
-  }
+  };
 
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-  document.addEventListener('keypress', function(event) {
-
-    if(event.keyCode == 13 || event.which == 13) {
-      ctrlAddItem();
+  return {
+    init: function() {
+      console.log('Application has starterd.');
+      setupEventListener();
     }
-
-  });
+  };
 
 })(budgetController, UIController);
+
+controller.init();
